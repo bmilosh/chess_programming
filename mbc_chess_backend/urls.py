5f68563f6e579b03c7cb6be414f13a6c78daed2a
bmilosh <mikkybams@gmail.com>
2023-06-22 19:51:16 +0200

    Tweaked urls

--- mbc_chess_backend/urls.py
+++ mbc_chess_backend/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("test_play/", views.make_move),
-    path("quit/", views.kill_engine),
+    path("quit", views.kill_engine),
     path("start_engine", views.start_engine),
     re_path("^(?:.*)?$", TemplateView.as_view(template_name="base.html")),
 ]
