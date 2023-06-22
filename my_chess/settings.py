b1c644d1adbd5fdbbed49fe0926f920060a60671
bmilosh <mikkybams@gmail.com>
2023-06-22 19:17:00 +0200

    Tweaked settings

--- my_chess/settings.py
+++ my_chess/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DJANGO_DEBUG_ON")
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "mb-chess-fcb92b9ccc46.herokuapp.com"]
 
