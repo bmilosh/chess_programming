9ed85cc9f4c3bcfda189e0d49965940cef8a8821
bmilosh <mikkybams@gmail.com>
2023-06-23 19:31:41 +0200

    Tweaked views

--- mbc_chess_backend/views.py
+++ mbc_chess_backend/views.py
@@ -13,21 +13,32 @@ from rest_framework.response import Response
 # Enable debug logging.
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
-# engine = chess.engine.SimpleEngine
-engine = None
+# # engine = chess.engine.SimpleEngine
+# engine = None
 
+if sys.platform == "linux":
+    os.chmod('./chess_engine/dist/MBC/MBC', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    # os.chmod('./chess_engine/dist/MBC/MBC', stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+    # statinfo = os.stat('./chess_engine/dist/MBC/MBC')
+    # print(statinfo)
+    engine = chess.engine.SimpleEngine.popen_uci('./chess_engine/dist/MBC/MBC')
+else:
+    engine = chess.engine.SimpleEngine.popen_uci('./chess_engine/dist1/MBC/MBC.exe')
+# python3 manage.py runserver 8080
 @api_view(["POST"])
 def make_move(request):
-    if engine is None:
-        return Response(json.dumps({"message": "Engine not running. Cannot make move."}))
+    # if engine is None:
+    #     return Response(json.dumps({"message": "Engine not running. Cannot make move."}))
     body = json.loads(request.body)
     # Extract relevant info from body
     fen = body["fen"]
     limit = body["searchTimeLimit"]
 
     board = chess.Board(fen=fen)
-    result = engine.play(board, chess.engine.Limit(time=float(limit)))
-    best_move = result.move
+    # result = engine.play(board, chess.engine.Limit(time=float(limit)))
+    # best_move = result.move
+    result = engine.analyse(board, chess.engine.Limit(time=float(limit)))
+    best_move = result["pv"][0]
     body["san"] = board.san(best_move)
     body["best_move"] = board.lan(best_move)
     body["square_from"] = chess.SQUARE_NAMES[best_move.from_square]
@@ -49,26 +60,26 @@ def make_move(request):
 
 @api_view(["GET"])
 def kill_engine(request):
-    # print("Kill engine called")
-    try:
-        engine.quit()
-        engine.close()
-    # except chess.engine.EngineTerminatedError:
-    except:
-        return Response(json.dumps({"message": "Engine already killed"}))
+    # # print("Kill engine called")
+    # try:
+    #     engine.quit()
+    #     engine.close()
+    # # except chess.engine.EngineTerminatedError:
+    # except:
+    #     return Response(json.dumps({"message": "Engine already killed"}))
     return Response(json.dumps({"message": "Engine successfully killed"}))
 
 
 @api_view(['GET'])
 def start_engine(request):
-    global engine
-    if sys.platform == "linux":
-        os.chmod('./chess_engine/dist/MBC/MBC', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-        # os.chmod('./chess_engine/dist/MBC/MBC', stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-        statinfo = os.stat('./chess_engine/dist/MBC/MBC')
-        # print(statinfo)
-        engine = chess.engine.SimpleEngine.popen_uci('./chess_engine/dist/MBC/MBC')
-    else:
-        engine = chess.engine.SimpleEngine.popen_uci('./chess_engine/dist1/MBC/MBC.exe')
+    # global engine
+    # if sys.platform == "linux":
+    #     os.chmod('./chess_engine/dist/MBC/MBC', stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    #     # os.chmod('./chess_engine/dist/MBC/MBC', stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+    #     statinfo = os.stat('./chess_engine/dist/MBC/MBC')
+    #     # print(statinfo)
+    #     engine = chess.engine.SimpleEngine.popen_uci('./chess_engine/dist/MBC/MBC')
+    # else:
+    #     engine = chess.engine.SimpleEngine.popen_uci('./chess_engine/dist1/MBC/MBC.exe')
     # engine = chess.engine.SimpleEngine.popen_uci('./chess_engine/dist1/MBC/MBC.exe')
     return Response(json.dumps({"message": "Engine started successfully"}))
